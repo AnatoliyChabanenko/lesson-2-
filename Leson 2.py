6a8c3cf5e61915f7b013efbddea03cb5b3e8193f
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2020-12-17 13:48:08 +0200

    text pravok

---
+++ Leson 2.py
@@ -0,0 +1,7 @@
+name = ("Anatolii")
+lastanme = ('Çhabanenko')
+dt = '17/12/2020'
+print('Good day',name, dt, 'is a perfect day to learn some python' )
+
+a= str(name+lastanme)
+print(a)
---
+++ test2.py
@@ -0,0 +1,8 @@
+a = (123)
+print('hi', a , sep="---", end="yvs" )
+
+b= ('#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########')
+print (b)
+
+h=('\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#')
+print (h)
